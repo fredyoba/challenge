689fa507f9ceaa74976b26c55abf9f4138c638f7
yoba fred <fred@yoba-freds-Mac-Pro.local>
2018-03-07 17:35:21 +0800

    challenge 2 update

--- src/main/java/com/totalnihon/ubs/connect4/Connect4.java
+++ src/main/java/com/totalnihon/ubs/connect4/Connect4.java
@@ -3,6 +3,7 @@ package com.totalnihon.ubs.connect4;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import com.totalnihon.ubs.connect4.infrastructure.exception.GameTerminatedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -28,24 +29,37 @@ public class Connect4 implements CommandLineRunner {
 	public void run(String... arg0) throws Exception {
 		int column;	
 		Scanner scanner = new Scanner(System.in);
+		boolean success = false;
 
+		ge.startNewGame();
 		ge.printBoard();
 		
 		do {
-		    //  prompt for the user's name
-		    System.out.print((ge.getTurn()? GameEngine.PLAYERA:GameEngine.PLAYERB) +  "[" + (ge.getTurn()? GameEngine.PLAYERA_COLOR:GameEngine.PLAYERB_COLOR) + "] - choose column (1-7): ");
-	
-		    // get their input as a String
-		    try {
-		    	column = scanner.nextInt();
+			//  prompt for the user's name
+			System.out.print((ge.getTurn()? GameEngine.PLAYERA:GameEngine.PLAYERB) +  "[" + (ge.getTurn()? GameEngine.PLAYERA_COLOR:GameEngine.PLAYERB_COLOR) + "] - choose column (1-7): ");
+
+			// get their input as a String
+			try {
+				success = false;
+				column = scanner.nextInt();
+
 				ge.play(ge.getTurn(), column);
-			    ge.printBoard();
-		    } catch(InputMismatchException ime) {
-		    	System.out.println(WRONG_INPUT);
-		    	scanner.next();
-		    } catch (ColumnIsFullException | ColumnOutOfBoudariesException e) {
+				success = true;
+			} catch(InputMismatchException ime) {
+				if("u".equalsIgnoreCase(scanner.next())) {
+					ge.undo();
+					success = true;
+				} else{
+					System.out.println(WRONG_INPUT);
+				}
+			} catch (ColumnIsFullException | ColumnOutOfBoudariesException | GameTerminatedException e) {
 				System.out.println(e.getMessage());
-			} 		    		    
+			} finally {
+				if(success) {
+					ge.printBoard();
+					ge.toggleTurn();
+				}
+			}
 		} while(!ge.isGameFinished());
 	
 		scanner.close();
